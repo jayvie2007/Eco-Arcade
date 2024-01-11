4b091d45d93e4a9eed0f647c3a9e2bd63d988625
Jayvie De Leon <123736031+jayvie2007@users.noreply.github.com>
2024-01-11 17:46:19 +0800

    add: while true

--- RaspberryPi/temp_humid.py
+++ RaspberryPi/temp_humid.py
@@ -28,38 +28,39 @@ device_two = max7219(serial_two, width=32, height=8, block_orientation=-90,block
 virtual_two = viewport(device_two, width=32, height=16)
 device_two.contrast(100)
 
-try:
-    temperature = dht.temperature
-    temp = ("T:{:.1f}".format(temperature))
-    humidity = dht.humidity
-    humid = f"H:{humidity}%"
+while True:
+    try:
+        temperature = dht.temperature
+        temp = ("T:{:.1f}".format(temperature))
+        humidity = dht.humidity
+        humid = f"H:{humidity}%"
 
-    #Check if humid and temp catches value else 0
-    if not humid:
-        humidty = 0
-        humid = ("H:0%")
-    if not temp:
-        temperature = 0
-        temp = ("T:0")
+        #Check if humid and temp catches value else 0
+        if not humid:
+            humidty = 0
+            humid = ("H:0%")
+        if not temp:
+            temperature = 0
+            temp = ("T:0")
 
-    #First Count
-    with canvas(virtual) as draw:
-        text(draw, (0, 1), temp, fill="white", font=proportional(LCD_FONT))
-    with canvas(virtual_two) as draw:
-        text(draw, (4, 1), humid, fill="white", font=proportional(LCD_FONT))
-    db().child("Weather").update({"temperature":temperature,"humidity":humidity})
+        #First Count
+        with canvas(virtual) as draw:
+            text(draw, (0, 1), temp, fill="white", font=proportional(LCD_FONT))
+        with canvas(virtual_two) as draw:
+            text(draw, (4, 1), humid, fill="white", font=proportional(LCD_FONT))
+        db().child("Weather").update({"temperature":temperature,"humidity":humidity})
 
-    time.sleep(5)
-    #Second Count
-    with canvas(virtual) as draw:
-        text(draw, (0, 1), temp, fill="white", font=proportional(LCD_FONT))
-    with canvas(virtual_two) as draw:
-        text(draw, (4, 1), humid, fill="white", font=proportional(LCD_FONT))
-    db().child("Weather").update({"temperature":temperature,"humidity":humidity})
+        time.sleep(5)
+        #Second Count
+        with canvas(virtual) as draw:
+            text(draw, (0, 1), temp, fill="white", font=proportional(LCD_FONT))
+        with canvas(virtual_two) as draw:
+            text(draw, (4, 1), humid, fill="white", font=proportional(LCD_FONT))
+        db().child("Weather").update({"temperature":temperature,"humidity":humidity})
 
-except RuntimeError as e:
-    print("Reading from DHT failure: ", e.args)
-    time.sleep(0.5)
-except KeyboardInterrupt:
-    GPIO.cleanup()
-    print("Stopped")
+    except RuntimeError as e:
+        print("Reading from DHT failure: ", e.args)
+        time.sleep(0.5)
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+        print("Stopped")
