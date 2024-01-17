180e449d45f6f7919d4a5dc254106d50b782fec4
Jayvie De Leon <123736031+jayvie2007@users.noreply.github.com>
2024-01-17 10:22:22 +0800

    adjust: matrix orientation

--- RaspberryPi/temp_humid.py
+++ RaspberryPi/temp_humid.py
@@ -18,13 +18,13 @@ dht = adafruit_dht.DHT22(board.D21, use_pulseio=False)
 
 #PIN DIN=19 LEFT CLK=23 LEFT CS=24 RIGHT
 serial = spi(port=0, device=0, gpio=noop())
-device = max7219(serial, width=32, height=8, block_orientation=-90, blocks_arranged_in_reverse_order=False)
+device = max7219(serial, width=32, height=8, block_orientation=90, blocks_arranged_in_reverse_order=True)
 device.contrast(100)
 virtual = viewport(device, width=32, height=16)
 
 #PIN DIN=19 LEFT CLK=23 LEFT CS=26 RIGHT
 serial_two = spi(port=0, device=1, gpio=noop())
-device_two = max7219(serial_two, width=32, height=8, block_orientation=-90,blocks_arranged_in_reverse_order=False)
+device_two = max7219(serial_two, width=32, height=8, block_orientation=90,blocks_arranged_in_reverse_order=True)
 virtual_two = viewport(device_two, width=32, height=16)
 device_two.contrast(100)
 
