5d9a99a2235d8fbb184acd4cca37115f116501cc
Jayvie De Leon <123736031+jayvie2007@users.noreply.github.com>
2024-02-18 21:50:25 +0800

    working lcd

--- Version2/LCD.ino
+++ Version2/LCD.ino
@@ -1,18 +1,20 @@
+#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-int counter = 0;
-LiquidCrystal_I2C lcd(0x3F, 16, 2); // Set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 void setup() {
-  lcd.init();                       // Initialize the LCD
-  lcd.backlight();                  // Turn on the backlight
-  lcd.clear();                      // Clear the LCD screen
+  lcd.begin(16, 2);
+  lcd.init();
+  lcd.backlight();
+  // Additional setup code
 }
 void loop() {
-  lcd.setCursor(0, 0);               // Set the cursor to the first column and first row
-  lcd.print("    INOVATRIX   ");     // Print some text
-  lcd.setCursor(0,1);
-  lcd.print(counter);
-  counter = counter + 1;
-  delay(500);                        // Wait for 1/2 seconds
-}
+  lcd.setCursor(0, 0);
+  lcd.print("AKI");
+  delay(1000);
 
-https://github.com/harshkzz/ESP8266-16x2LCD-I2C/blob/main/ESP32%20i2C%20LCD%2016x2.png
+  lcd.setCursor(0, 1);
+  lcd.print("");
+  delay(800);
+
+  lcd.clear();
+}
