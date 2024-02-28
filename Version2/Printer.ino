2c03bf7a9bef123c6a5073996041784f70928619
Jayvie De Leon <123736031+jayvie2007@users.noreply.github.com>
2024-02-28 13:43:23 +0800

    add: code for printer

--- Version2/Printer.ino
+++ Version2/Printer.ino
@@ -10,29 +10,55 @@
 #define WIFI_SSID "OPPO A92"
 #define WIFI_PASSWORD "11111111"
 
-#define TX_PIN 6 // Arduino TX pin -- RX on printer
-#define RX_PIN 5 // Arduino RX pin -- TX on printer
-
-SoftwareSerial printer_connection(RX_PIN, TX_PIN);
-Adafruit_Thermal printer(&printer_connection);
+int is_switch_press = 0; // For detecting the switch press status
+int debounce_delay = 300; //Debounce delay
+int switch_button = D1;
 
 void setup()
 {  
-  printer_connection.begin(9600);
-  printer.begin();
-
-  printer.setFont('B');
-  printer.println("Hello, World!");
-  printer.setFont('A');
-  printer.println("Hello, World!");
-
-  printer.inverseOn();
-  printer.println(F("Good Bye, World!"));
-  printer.inverseOff();
-
-  printer.doubleHeightOn();
-  printer.println(F("Large Text"));
-  printer.doubleHeightOff();
+  pinMode(switch_button, INPUT);
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
 }
 
-void loop() { }
+void loop() {
+  is_switch_press = digitalRead(switch_button); // Reading the Switch press status
+  
+  String line1 = Firebase.getString("Printer/Line1")
+  String line2 = Firebase.getString("Printer/Line2")
+  String line3 = Firebase.getString("Printer/Line3")
+  String line4 = Firebase.getString("Printer/Line4")
+  String line5 = Firebase.getString("Printer/Line5")
+  String printer_starter = Firebase.getString("Printer/Start")
+
+  if (is_switch_press == HIGH){
+      delay(debounce_delay); // debounce delay for button press
+      if (printer_starter == "start"){
+        if (is_switch_press == HIGH){
+          Serial.println(line1);
+          Serial.println("---------------------------- \n\r");
+          Serial.println("\n\r");
+          Serial.println("\n\r");
+          Serial.println("\n\r");
+          delay(100);
+          Serial.println(line2);
+          Serial.println(line3);
+          delay(100);
+          Serial.println("\n\r");
+          Serial.println("\n\r");
+          Serial.println("---------------------------- \n\r");
+          Serial.println(line4);
+          Serial.println(line5);
+          Serial.println("Thank You.");
+          Serial.println("\n\r");
+          Serial.println("\n\r");
+          Serial.println("\n\r");
+          Firebase.setString("Printer/Start","stop");
+        }    
+    }
+    else{
+      Firebase.setString("BinResponse/Message","Insert a bottle first");
+      delay(1000)
+      Firebase.setString("BinResponse/Message","");
+    }
+  }
+}
